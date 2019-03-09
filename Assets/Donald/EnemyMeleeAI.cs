7b81745d8862853f995cf72372dd293442fb8755
Donald Thatcher <donald.thatcher@outlook.com>
2019-03-09 09:27:22 -0500

    Updated AI melee
    added more lines to Ai for detecting player if player is in feild of view

--- Assets/Donald/EnemyMeleeAI.cs
+++ Assets/Donald/EnemyMeleeAI.cs
@@ -15,6 +15,7 @@ public class EnemyMeleeAI : MonoBehaviour {
     private PlayerHealth playerHealth;
     private GameObject player;
     private Vector3 previousSighting;
+    private HashIDs hash;
 
 	// Use this for initialization
 	void Start () {
@@ -45,7 +46,34 @@ public class EnemyMeleeAI : MonoBehaviour {
 
         previousSighting = lastPlayerSighting.position;
 
-        if(playerHealth.health > 0f)
-            anim.SetBool()
-	}
+        if (playerHealth.health > 0f)
+            anim.SetBool(hash.playerInSightBool, playerInSight);
+        else
+            anim.SetBool(hash.playerInSightBool, false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInSight = false;
+
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            if(angle < fieldOfViewAngle * 0.5f)
+            {
+                RaycastHit hit;
+
+                if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+                {
+                    if(hit.collider.gameObject == player)
+                    {
+                        playerInSight = true;
+                        lastPlayerSighting.position = player.transform.position;
+                    }
+                }
+            }
+        }
+    }
 }
