042a917d2bf100f264ab289e33750a8cdf982982
lolerooz <rdalbarran@hotmail.com>
2019-03-10 00:51:02 -0500

    Animated Melee, still need to program collision

---
+++ Assets/Ruben/Scripts/Melee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+
+    Animator animator;
+    public Collider coll;
+
+    // Use this for initialization
+    void Start () {
+
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                animator.SetBool("meleeStart", true);
+            }
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Melee")) {
+            animator.SetBool("meleeStart", false);
+        }
+
+    }
+}
