96daff82c65e8d08386237f1ec8160a63ad56cb2
Donald Thatcher <donald.thatcher@outlook.com>
2019-03-17 20:40:13 -0400

    Update Gates and AI
    Added animation for gates and added a collider for the tower in middle. place spawn point for enemies at north gate.

---
+++ Assets/Donald/EnemyScripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class Enemy : MonoBehaviour {
+
+
+    public int health;
+    public int speed;
+    public int visonRange;
+    public int attackRange;
+    public int agrroRange;
+    public Movement player;
+    public 
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}*/
