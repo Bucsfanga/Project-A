710128aa70a9c71f4daa5ace7a2c044f0b465d8f
Donald Thatcher <donald.thatcher@outlook.com>
2019-03-26 11:34:28 -0400

    added coin box
    Added loot mechnics

---
+++ Assets/Donald/Scripts/Loot.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Loot : MonoBehaviour {
+
+//    public int coins;
+//    public int healed;
+
+//    HealthBarSystem GetHealth.regan;
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.tag == "Player")
+//        {
+//            PlayerStats.playerStats.coin += coins;
+//            HealthBarSystem 
+//        }
+//    }
+//}
