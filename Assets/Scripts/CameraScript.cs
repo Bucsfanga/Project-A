1b5147ad30c9172b6c781119c5f5b3b3740b487a
Donald Thatcher <donald.thatcher@outlook.com>
2019-07-18 06:09:27 -0400

    Rename Files and Heiarchy
    Change Scrpits for player movement to reflect new naming convention.
    Added the player model with color
    Rename scnces to reflect new naming convention
    New error of camera nda player body

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
 
 
-    public float minY = -45.0f;
-    public float maxY = 45.0f;
+    public float minY = 90.0f;
+    public float maxY = 140.0f;
 
-    public float sensX = 100.0f;
+    public float sensX = 150.0f;
     public float sensY = 100.0f;
 
     public float rotationX = 0.0f;
@@ -15,12 +17,12 @@ public class CameraScript : MonoBehaviour {
 
     public float offset = 1.5f;
 
-    public GameObject target;
+    //public GameObject target;
 
     void Update()
     {
 
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y+ offset, target.transform.position.z);
+        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y+ offset, target.transform.position.z);
         rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
         rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
         rotationY = Mathf.Clamp(rotationY, minY, maxY);
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float translateSpeed = 40.0f;
+    public float rotateSpeed = 20.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime * 10);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * 10);
+
+
+        if (controller.isGrounded)
+        {
+
+            moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));  
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * translateSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
